18ee0aa09dca52cdc202e91ef7cb8f4242db1fa8
raisatya <satyanlpid1@gmail.com>
2024-06-05 21:50:11 +0530

    Nextauth initial commit 4

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,12 +1,11 @@
 "use client"
-import React from 'react'
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center p-3 bg-transparent border-b w-full">
       <div className="flex items-center md:gap-x-12">
         <button className="w-32 sm:w-36">
-          <img src="/next.svg" alt="Dlogo" />
+          <img src="/TypeRush.svg" alt="Dlogo" className="-mt-5"/>
         </button>
       </div>
       <button className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold bg-blue-600 text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 outline-none">
